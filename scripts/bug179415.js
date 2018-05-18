6bce224a6cbb28c9b12848a598ad3b91a6c1fd5a
Rafael Rivero <rafael_rivero@Riveros-MacBook-Pro.local>
2018-05-18 21:10:32 +0200

    Simplified sample script with underscore

--- scripts/bug179415.js
+++ scripts/bug179415.js
@@ -1,66 +1,49 @@
 // mmcollect -u xxxx -h xxxx -s bug179415.js "show ip interface brief; show datapath session table"
 
 // "data0" espera el resultado de "show ip interface brief"
-data0 = data0.data || data0;
+data0 = data0._data || data0;
 
-// Obtengo los prefijos de todas las interfaces de la controladora
-var interfaces = new Array();
-var redes = new Array();
-for (var i = 0; i < data0.length; i++) {
-	var line = data0[i]
-	if (line.indexOf("vlan") >= 0) {
-		// El tercer campo es la IP de la interfaz
+// Obtengo las direcciones IP de todas las interfaces de la controladora
+var interfaces = _.reduce(data0, function (memo, line) {
+	if (line.indexOf("vlan ") >= 0) {
+		// El tercer campo de esta línea debería ser la dirección IP de la interfaz
 		var ip = line.match(/\S+/g)[2];
-		var red = ip.substr(0, ip.lastIndexOf(".") + 1);
-		// safety check
-		if (red.length > 8) {
-			interfaces.push(ip);
-			redes.push(red);
+		// Compruebo que parece una IP
+		if (/^[0-9\.]{7,}$/.test(ip)) {
+			memo.push(ip);
 		}
 	}
-}
+	return memo;
+}, []);
+var redes = _.map(interfaces, function(ip) {
+	// Simplificando, no tengo en cuenta la mascara
+	return ip.substr(0, ip.lastIndexOf(".") + 1);
+});
 
 // Devuelve true si la IP esta en la red
 function enRed(ip, redes) {
-	for (var i = 0; i < redes.length; i++) {
-		var red = redes[i];
-		if (ip.indexOf(red) == 0) return true;
-	}
-	return false;
+	return _.some(redes, function(red) { return ip.indexOf(red) == 0; })
 }
 
-// data1 espera el resultado de "show datapath session table"
-data1 = data1.data || data1;
-var corrupt = new Array();
-var users = {};
-for (var i = 0; i < data1.length; i++) {
-	var line = data1[i]
-	if (line.indexOf("nh 0x") >= 0) {
+// "data1" espera el resultado de "show datapath session table"
+data1 = data1._data || data1;
+
+// Obtengo la lista de entradas e IPs afectadas por el problema en esta controladora
+users = {};
+corrupt = _.filter(data1, function(line) {
+	if (line.indexOf("nh 0x") > 0) {
 		// primer y segundo campo son IP origen y destino
-		var partes = line.match(/\S+/g);
-		origen = partes[0];
-		destino = partes[1];
+		var ips = line.match(/\S+/g).slice(0, 2);
 		// Es una entrada corrupta si el origen y el destino son redes locales
-		if (enRed(origen, redes) && enRed(destino, redes)) {
-			corrupt.push(line);
-			// No tengo en cuenta las IPs de la propia controladora
-			if (!enRed(origen, interfaces)) {
-				users[origen] = 1;
-			}
-			if (!enRed(destino, interfaces)) {
-				users[destino] = 1;
-			}
+		if (_.every(ips, function (ip) { return enRed(ip, redes); })) {
+			_.each(ips, function(ip) { users[ip] = 1; });
+			return true;
 		}
+		return false;
 	}
-}
+});
 
 // Manda el user delete a la controladora
-for (var key in users) {
-	if (users.hasOwnProperty(key)) {
-		var result = Post("/md", "object/aaa_user_delete", { "ipaddr": key });
-		corrupt.push("SENT aaa_user_delete FOR "+key+": "+result);
-	}
-}
-
-// El resultado del script son las entradas corruptas
-corrupt;
+corrupt.concat(_.map(_.keys(users), function(ip) {
+	return "SENT aaa_user_delete FOR " + ip + ": " + Post("/md", "object/aaa_user_delete", { "ipaddr": ip });
+}));
